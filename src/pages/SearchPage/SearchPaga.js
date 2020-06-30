119cfbacf90905c8bd4e54f8e67026563b75f68a
ALIFKI <mockingbirds233@gmail.com>
2020-06-30 08:40:50 +0700

    add search page

---
+++ src/pages/SearchPage/SearchPaga.js
@@ -0,0 +1,15 @@
+import React,{ Component } from 'react';
+
+class SearchPage extends Component {
+    render() {
+        return (
+            <div>
+                <h3>
+                    ini Search page
+                </h3>
+            </div>
+        )
+    }
+}
+
+export default SearchPage
